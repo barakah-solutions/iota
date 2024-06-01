1d3fbff680e3bf7d91ce8697aed9d2b4977ae2b8
Mansoor Ahmed Memon <mansoormemon@bss.com.pk>
2024-06-01 19:34:07 +0500

    Add services section

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import { Component } from "solid-js";
+import { Component, For, createSignal } from "solid-js";
 
 import HeroIllustration from "@assets/illustration-office-environment.svg";
 
@@ -41,10 +41,75 @@ const HeroSection: Component<{ id: string }> = ({ id }) => {
                         simple: to pioneer sustainable solutions that redefine industry standards and lead the charge
                         towards a smarter, more sustainable future.
                     </p>
+                    <div class="flex gap-8 items-center">
+                        <a
+                            href="#services"
+                            class="px-8 py-2 rounded-lg bg-brand-jet-black hover:bg-neutral-800 active:bg-neutral-950 font-quicksand font-semibold text-sm text-white duration-300 disabled:pointer-events-none disabled:opacity-50"
+                        >
+                            Learn More
+                        </a>
+                    </div>
                 </div>
                 <div class="flex-1 flex flex-row lg:flex-col justify-center duration-300">
                     <img src={HeroIllustration} class="w-full md:w-4/5 lg:w-full duration-300" />
                 </div>
+            </div>
+        </section>
+    );
+};
+
+const ServicesSection: Component<{ id: string }> = ({ id }) => {
+    const [availableServices] = createSignal([
+        {
+            title: "Web Development",
+            text: "Craft responsive and scalable websites tailored to your business needs.",
+        },
+        {
+            title: "Mobile Development",
+            text: "Create innovative and user-friendly mobile applications for iOS and Android platforms.",
+        },
+        {
+            title: "API Development",
+            text: "Build robust and secure APIs to power your web and mobile applications.",
+        },
+        {
+            title: "Database Solutions",
+            text: "Design and implement efficient database solutions to store and manage your data.",
+        },
+        {
+            title: "AI/ML Integration",
+            text: "Integrate cutting-edge artificial intelligence and machine learning technologies into your applications.",
+        },
+        {
+            title: "Cloud-Based Solutions",
+            text: "Deploy scalable and reliable cloud-based solutions to optimize your business operations and reduce costs.",
+        },
+        {
+            title: "UI/UX Design",
+            text: "Create intuitive and visually appealing user interfaces for a seamless user experience.",
+        },
+        {
+            title: "E-commerce Solutions",
+            text: "Build customized e-commerce platforms to drive sales and expand your online business.",
+        },
+    ]);
+
+    return (
+        <section id={id} class="flex flex-col gap-8 py-32 lg:py-48 duration-300">
+            <h3 class="font-semibold text-lg text-gray-800 leading-loose uppercase underline underline-offset-4 decoration-2 decoration-double decoration-brand-olive-green">
+                Services
+            </h3>
+            <div class="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-12">
+                <For each={availableServices()}>
+                    {(service, _) => {
+                        return (
+                            <div class="flex flex-col gap-2 p-6 rounded-xl bg-gray-100 hover:bg-gray-200/70 overflow-hidden cursor-pointer transition-all">
+                                <h3 class="font-semibold text-lg text-gray-800 leading-6">{service.title}</h3>
+                                <p class="text-sm text-gray-500">{service.text}</p>
+                            </div>
+                        );
+                    }}
+                </For>
             </div>
         </section>
     );
@@ -57,6 +122,7 @@ const HomePage = () => {
             <div class="max-w-screen-2xl mx-auto">
                 <main class="px-8">
                     <HeroSection id="hero" />
+                    <ServicesSection id="services" />
                 </main>
             </div>
         </>
